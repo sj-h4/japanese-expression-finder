24e167fce1eb924e150f3c306b8809de1ef95ef5
sj <64534668+sj-h4@users.noreply.github.com>
2022-11-27 18:49:38 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use lindera::tokenizer::{Token, Tokenizer};
 use std::{
-    cmp::max,
+    cmp::{max, min},
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -13,12 +13,27 @@ fn tokenize(target_text: &String) -> Vec<Token> {
     return tokens;
 }
 
+#[test]
+fn test_tokenize() {
+    let target_text = "今日も晴天です".to_string();
+    let tokens = tokenize(&target_text);
+    for token in &tokens {
+        println!("{:?}", token.detail);
+    }
+    assert_eq!(tokens.len(), 4);
+}
+
 fn find_expression(search_range: isize, tokens: &Vec<Token>) -> Vec<String> {
     let mut expressions = Vec::new();
     let orth_token_index = 6;
     let token_length = tokens.len();
     for (i, value) in tokens.iter().enumerate() {
-        if value.text != "は" || i == token_length - 1 {
+        let word_category;
+        match value.detail.get(1) {
+            Some(v) => word_category = v,
+            None => continue,
+        }
+        if word_category != "係助詞" || i == token_length - 1 {
             continue;
         }
         let target_word;
@@ -35,7 +50,8 @@ fn find_expression(search_range: isize, tokens: &Vec<Token>) -> Vec<String> {
             }
             if word == target_word {
                 let mut found_expression = String::from("");
-                for k in j..i + 2 {
+                let end_index = min(i + 5, token_length);
+                for k in j..end_index {
                     found_expression += tokens[k].text;
                 }
                 expressions.push(found_expression);
