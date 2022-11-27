e431e32afcff45757082c66f7165318ea55f9c68
sj <64534668+sj-h4@users.noreply.github.com>
2022-11-27 14:44:12 +0900

    抽出できるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use lindera::tokenizer::{Token, Tokenizer};
+use std::cmp::max;
 
 fn tokenize(target_text: &String) -> Vec<Token> {
     let tokenizer = Tokenizer::new().unwrap();
@@ -8,11 +9,37 @@ fn tokenize(target_text: &String) -> Vec<Token> {
     return tokens;
 }
 
+fn find_expression(search_range: usize, tokens: &Vec<Token>) -> Vec<String> {
+    let mut expressions = Vec::new();
+    let orth_token_index = 6;
+    let token_length = tokens.len();
+    for (i, value) in tokens.iter().enumerate() {
+        if value.text != "は" || i == token_length - 1 {
+            continue;
+        }
+        let target_word = tokens[i + 1].detail.get(orth_token_index).unwrap();
+        let start_index = max(0, i - search_range);
+        for j in start_index..i {
+            let word = tokens[j].detail.get(orth_token_index).unwrap();
+            if word == target_word {
+                let mut found_expression = String::from("");
+                for k in j..i + 2 {
+                    found_expression += tokens[k].text;
+                }
+                expressions.push(found_expression);
+                continue;
+            }
+        }
+    }
+    return expressions;
+}
+
 fn main() {
-    let target_text = String::from("とりあえず読むには読んだが、あまりにも難しい。");
-    let tokens = tokenize(&target_text);
-    for token in tokens {
-        println!("{}", token.text);
-        println!("{:?}", token.detail);
+    let target_text = String::from(
+        "とりあえず読むには読んだが、あまりにも難しい。早いは早いがあまり正確ではない。",
+    );
+    let expressions = find_expression(3, &tokenize(&target_text));
+    for expression in expressions {
+        println!("{}", expression);
     }
 }
