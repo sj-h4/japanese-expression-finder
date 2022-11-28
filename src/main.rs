9ed66eeeb1c2ee4963fae63eeb161c33cbb7c9c2
sj <64534668+sj-h4@users.noreply.github.com>
2022-11-29 02:13:18 +0900

    範囲を広げた

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn tokenize(target_text: &String) -> Vec<Token> {
 
 #[test]
 fn test_tokenize() {
-    let target_text = "今日も晴天です".to_string();
+    let target_text = "仲の直るのもまた早いには早いが。".to_string();
     let tokens = tokenize(&target_text);
     for token in &tokens {
         println!("{:?}", token.detail);
@@ -23,7 +23,8 @@ fn test_tokenize() {
     assert_eq!(tokens.len(), 4);
 }
 
-fn find_expression(search_range: isize, tokens: &Vec<Token>) -> Vec<String> {
+fn find_expression(search_range: usize, tokens: &Vec<Token>) -> Vec<String> {
+    let conjections = ["が", "けれど", "けど", "ども", "も", "しかし", "だが"];
     let mut expressions = Vec::new();
     let orth_token_index = 6;
     let token_length = tokens.len();
@@ -41,8 +42,24 @@ fn find_expression(search_range: isize, tokens: &Vec<Token>) -> Vec<String> {
             Some(v) => target_word = v,
             None => continue,
         };
-        let start_index = max(0, i as isize - search_range) as usize;
+        let start_index = max(0, i as isize - search_range as isize) as usize;
+        let end_index = min(i + search_range + 3, token_length);
+
+        // HACK: もう少しすっきり書けるはず
+        let mut has_conjection = false;
+        for word_index in i + 1..end_index {
+            let word = tokens[word_index].text;
+            if conjections.contains(&word) {
+                has_conjection = true;
+                break;
+            }
+        }
+        if !has_conjection {
+            continue;
+        }
+
         for j in start_index..i {
+            // 述語が重複しているかを調べる
             let word;
             match tokens[j].detail.get(orth_token_index) {
                 Some(v) => word = v,
@@ -50,7 +67,6 @@ fn find_expression(search_range: isize, tokens: &Vec<Token>) -> Vec<String> {
             }
             if word == target_word {
                 let mut found_expression = String::from("");
-                let end_index = min(i + 5, token_length);
                 for k in j..end_index {
                     found_expression += tokens[k].text;
                 }
